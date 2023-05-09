707db1e5aa574f63a47b809555a869fdfb52df70
mika <1506612+elmika@users.noreply.github.com>
2023-05-09 14:13:47 +0200

    Set up new launch creation.

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,5 +1,7 @@
 const launches = new Map();
 
+let latestFlightNumber = 100;
+
 const launch = {
     flightNumber: 100,
     mission: 'Kepler Exploration X',
@@ -17,6 +19,20 @@ function getAllLaunches() {
     return Array.from(launches.values());
 }
 
+function addNewLaunch(launch){
+    latestFlightNumber += 1;
+    launches.set(
+        latestFlightNumber, 
+        Object.assign(launch, {
+            customer: ["Zero to Mastery", "NASA"],
+            flightNumber: latestFlightNumber,
+            upcoming: true,
+            success: true,
+        })
+    );
+}
+
 module.exports = {
     getAllLaunches,
+    addNewLaunch,
 }
--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -1,9 +1,20 @@
-const { getAllLaunches } = require('../../models/launches.model');
+const {
+    getAllLaunches,
+    addNewLaunch 
+} = require('../../models/launches.model');
 
 function httpGetAllLaunches(req, res) {
     return res.status(200).json(getAllLaunches());
 }
 
+function httpAddNewLaunch(req, res) {
+    const launch = req.body;
+    launch.launchDate = new Date(launch.launchDate);
+    id = addNewLaunch(launch);
+    return res.status(201).json(launch); // Pending: add Location header            
+}
+
 module.exports = {
     httpGetAllLaunches,
+    httpAddNewLaunch,
 };
